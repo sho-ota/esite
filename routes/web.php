8786d2a712047c6919a84bb7981c0f6d4d580601
sho.ota <sho.ota.art.567@gmail.com>
2020-11-23 15:04:29 +0000

    menntarinngu 1kaime syuuryougo

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,10 @@
 |
 */
 
-/*
+// MEMO: トップページにアクセスされたら、welcome.blade.phpを表示するという記述
 Route::get('/', function () {
     return view('welcome');
 });
-*/
-
-
-/*
-// welcomeを表示
-Route::namespace('User')->group(function () {
-    Route::get('/', 'HomeController@index'); 
-});
-*/
-
-
-/*
-Route::namespace('Staff')->group(function () {
-    Route::get('/', 'HomeController@index'); 
-});
-*/
-
 
 // 利用者
 Route::namespace('User')->prefix('user')->name('user.')->group(function () {
@@ -67,7 +50,25 @@ Route::namespace('Staff')->prefix('staff')->name('staff.')->group(function () {
 
         // TOPページ
         Route::resource('home', 'HomeController', ['only' => 'index']);
-
+        
+        // MEMO: ここでスタッフ追加や利用者追加の処理を担うコントローラーのアクションを呼び出す
+        
+        // Staff
+        // MEMO: /staff/staffsでスタッフ一覧を表示
+        // Route::get('staffs', 'StaffsController@index');
+        // MEMO: /staff/staffs/createでスタッフ作成フォーム画面を表示
+        // Route::get('staffs/create', 'StaffsController@create');
+        
+        // User
+        // MEMO: /staff/usersでスタッフ一覧を表示
+        // Route::get('users', 'UsersController@index');
+        // MEMO: /staff/users/createでスタッフ作成フォーム画面を表示
+        // Route::get('users/create', 'UsersController@create');
+        
+        // TODO: やること
+        // 1. larave/uiをいれる
+        // 2. StaffsControllerとUsersControllerを作成する
+        // 2のコントローラーにindexとcreate、storeアクションを実装して、処理を記述する
     });
 
 });
