2c46bcb7e815b32c7590928f641eecc0d8e8887a
sho.ota <sho.ota.art.567@gmail.com>
2020-11-23 13:08:09 +0000

    login and register url OK

---
+++ resources/views/staff/auth/register.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.staff.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>Sign up</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+
+           
+        </div>
+    </div>
+@endsection
---
+++ resources/views/user/auth/register.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.user.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>Sign up</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+
+           
+        </div>
+    </div>
+@endsection
